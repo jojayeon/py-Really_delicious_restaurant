7d27babccbce2966b67c996d55c855bd78884deb
jojayeon <jojayeon6152@gmail.com>
2024-09-24 14:30:33 +0900

    api자료를 가지고 데이터 크롤링

--- project/kcrawler/bs4selenium.py
+++ project/kcrawler/bs4selenium.py
@@ -30,34 +30,29 @@ results = []
 # 식당 이름으로 검색
 for restaurant in restaurants:  # 리스트를 순회
     name = restaurant['restrntNm']  # 식당 이름
-    phone = restaurant['restrntInqrTel']  # 식당 전화번호
     print(name)
     
     search_url = f'https://map.kakao.com/?q={name}'
     driver.get(search_url)
     
-    time.sleep(3)  # 페이지 로딩 대기
+    time.sleep(1)  # 페이지 로딩 대기
 
     try:
         # 검색 결과가 나올 때까지 대기
-        time.sleep(3)
+        time.sleep(1)
         
-        # 검색 결과의 모든 상세보기 링크 찾기
-        moreview_links = driver.find_elements(By.CSS_SELECTOR, "#info\\.search\\.place\\.list > li > div.info_item > div.contact.clickArea > a.moreview")
+        # 검색 결과의 첫 번째 상세보기 링크 찾기
+        moreview_link = driver.find_element(By.CSS_SELECTOR, "#info\\.search\\.place\\.list > li > div.info_item > div.contact.clickArea > a.moreview")
         
-        for link in moreview_links:
-            # 전화번호 가져오기
-            phone_element = link.find_element(By.XPATH, "..//span[@data-id='phone']")
-            if phone_element.text == phone:  # 전화번호 비교
-                link.click()  # 일치하는 링크 클릭
-                break  # 일치하는 링크 클릭 후 루프 종료
+        # 첫 번째 상세보기 링크 클릭
+        moreview_link.click()
         
-        time.sleep(2)  # 로딩 대기
+        time.sleep(1)  # 로딩 대기
 
         # 새 탭으로 전환
         driver.switch_to.window(driver.window_handles[-1])
         
-        time.sleep(2)  # 페이지 로딩 대기
+        time.sleep(1)  # 페이지 로딩 대기
 
         # 평점 가져오기
         rating_element = driver.find_element(By.CSS_SELECTOR, "#mArticle > div.cont_evaluation > div.ahead_info > div > em.num_rate")
@@ -65,7 +60,17 @@ for restaurant in restaurants:  # 리스트를 순회
 
         # 모든 리뷰 가져오기
         reviews = []
+        for _ in range(10):  # 최대 10번 시도
+            try:
+                more_reviews_button = driver.find_element(By.CSS_SELECTOR, "#mArticle > div.cont_evaluation > div.evaluation_review > a > span.txt_more")
+                more_reviews_button.click()
+                time.sleep(1)  # 클릭 후 로딩 대기
+            except Exception:
+                break  # 버튼이 더 이상 존재하지 않으면 종료
+
+        # 리뷰 가져오기
         review_elements = driver.find_elements(By.CSS_SELECTOR, "#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > p > span")
+        
         
         for review_element in review_elements:
             reviews.append(review_element.text)
